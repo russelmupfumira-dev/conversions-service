2386152705f86c97b2394551216527039ebab1a6
Russel Mupfumira <russel@jmr.co.za>
2020-08-04 14:40:14 +0200

    Initial project upload

---
+++ src/main/java/com/interview/playsafe/ConversionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.interview.playsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversionsServiceApplication.class, args);
+    }
+
+}
